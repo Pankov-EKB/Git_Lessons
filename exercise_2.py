7fc671a7795f77af55694628fbe7d8d80b6755bc
Pankov-EKB <67532947+Pankov-EKB@users.noreply.github.com>
2020-06-28 14:34:20 +0500

    Add files via upload

---
+++ exercise_2.py
@@ -0,0 +1,7 @@
+a = int(input('Введите  время в секундах:'))
+hours = a // 3600
+minutes = (a - hours * 3600) // 60
+seconds = a - (hours * 3600 + minutes * 60)
+print(f"Время в формате чч:мм:сс -  {hours} : {minutes} : {seconds}")
+
+
